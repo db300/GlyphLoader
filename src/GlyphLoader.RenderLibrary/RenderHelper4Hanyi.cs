6c7a2618d323ec8ea165ccbfeea53729d67c0288
DavidHawkes <db300@163.com>
2022-12-23 17:10:04 +0800

    create render library

---
+++ src/GlyphLoader.RenderLibrary/RenderHelper4Hanyi.cs
@@ -0,0 +1,92 @@
+﻿using System.IO;
+using System.Text;
+using WaterTrans.GlyphLoader;
+
+namespace GlyphLoader.RenderLibrary
+{
+    /// <summary>
+    /// 渲染帮助类(汉仪)
+    /// </summary>
+    public static class RenderHelper4Hanyi
+    {
+        /// <summary>
+        /// 汉仪官网字体详情页头图
+        /// </summary>
+        /// <param name="fileName">字体文件名</param>
+        /// <returns>头图svg字符串</returns>
+        public static string HanyiWebsiteFontDetailBanner(string fileName)
+        {
+            Typeface tf;
+            using (var fs = File.OpenRead(fileName))
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                tf = new Typeface(fs);
+            }
+
+            var row = arr.GetLength(0);//const int row = 4;
+            var col = arr.GetLength(1);//const int col = 12;
+            var width = col * em + (col - 1) * spaceX;
+            var height = (row + 1) * em + row * spaceY;
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"<svg width='{width}' height='{height}' viewBox='0 0 {width} {height}' xmlns='http://www.w3.org/2000/svg' version='1.1'>");
+
+            //渲染中文
+            for (var i = 0; i < row; i++)
+            {
+                for (var j = 0; j < col; j++)
+                {
+                    var glyphIndex = tf.CharacterToGlyphMap[arr[i, j][0]];
+                    var geometry = tf.GetGlyphOutline(glyphIndex, em);
+                    var baseline = tf.Baseline * em;
+                    var mini = geometry.Figures.ToString(j * (em + spaceX), i * (em + spaceY) + baseline);
+                    sb.AppendLine($"<path d='{mini}' fill='{color}' stroke='{color}' stroke-width='0' />");
+                }
+            }
+
+            //渲染英文数字
+            var x = 0D;
+            var y = row * (em + spaceY);
+            foreach (var c in s1)
+            {
+                var glyphIndex = tf.CharacterToGlyphMap[c];
+                var geometry = tf.GetGlyphOutline(glyphIndex, em);
+                var advanceWidth = tf.AdvanceWidths[glyphIndex] * em;
+                var baseline = tf.Baseline * em;
+                var mini = geometry.Figures.ToString(x, y + baseline);
+                sb.AppendLine($"<path d='{mini}' fill='{color}' stroke='{color}' stroke-width='0' />");
+                x += advanceWidth;
+            }
+
+            //渲染标点符号
+            var length = s2.Length;
+            x = width - length * em - (length - 1) * spaceX;
+            for (var i = 0; i < length; i++)
+            {
+                var glyphIndex = tf.CharacterToGlyphMap[s2[i]];
+                var geometry = tf.GetGlyphOutline(glyphIndex, em);
+                var baseline = tf.Baseline * em;
+                var mini = geometry.Figures.ToString(x + i * (em + spaceX), y + baseline);
+                sb.AppendLine($"<path d='{mini}' fill='{color}' stroke='{color}' stroke-width='0' />");
+            }
+
+            sb.AppendLine("</svg>");
+            System.Diagnostics.Debug.WriteLine(sb);
+            return sb.ToString();
+        }
+
+        private static readonly string[,] arr = new[,]
+        {
+            {"玉", "金", "露", "云", "律", "闰", "秋", "寒", "辰", "日", "宇", "天"},
+            {"出", "生", "结", "腾", "吕", "余", "收", "来", "宿", "月", "宙", "地"},
+            {"昆", "丽", "为", "致", "调", "成", "冬", "暑", "列", "盈", "洪", "玄"},
+            {"冈", "水", "霜", "雨", "阳", "岁", "藏", "往", "张", "昃", "荒", "黄"}
+        };
+        private const string s1 = "ABCabc123";
+        private const string s2 = "，。：；！？";
+        private const int spaceX = 5;
+        private const int spaceY = 1;
+        private const double em = 30;
+        private const string color = "white";//black
+    }
+}
--- src/MyTestConsole/Program.cs
+++ src/MyTestConsole/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text;
-using WaterTrans.GlyphLoader;
 
 namespace MyTestConsole
 {
@@ -9,59 +8,18 @@ namespace MyTestConsole
         {
             Console.WriteLine("Hello, World!");
 
-            double unit = 16;
-            double x = 20;
+            const string fontFileName = @"C:\Users\冷怀晶\Downloads\HYXiaoSongRongW.ttf";
+            const string svgFileName = $"{fontFileName}.svg";
 
-            Typeface tf;
-            using (var fs = File.OpenRead(@"C:\Users\冷怀晶\Downloads\HYDingXiaoDianW.ttf"))
+            var s = GlyphLoader.RenderLibrary.RenderHelper4Hanyi.HanyiWebsiteFontDetailBanner(fontFileName);
+
+            using (var sw = new StreamWriter(svgFileName))
             {
-                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-                tf = new Typeface(fs);
+                sw.Write(s);
+                sw.Flush();
             }
-
-            var arr = new[,]
-            {
-                {"冈", "水", "霜", "雨", "阳", "岁", "藏", "往", "张", "昃", "荒", "黄"},
-                {"昆", "丽", "为", "致", "调", "成", "冬", "暑", "列", "盈", "洪", "玄"},
-                {"出", "生", "结", "腾", "吕", "余", "收", "来", "宿", "月", "宙", "地"},
-                {"玉", "金", "露", "云", "律", "闰", "秋", "寒", "辰", "日", "宇", "天"}
-            };
-            const string s1 = "ABCabc123";
-            const string s2 = "，。：；！？";
-
-            const int spaceX = 5;
-            const int spaceY = 1;
-            const int em = 30;
-
-            var row = arr.GetLength(0);//const int row = 4;
-            var col = arr.GetLength(1);//const int col = 12;
-            var sb = new StringBuilder();
-            for (var i = 0; i < row; i++)
-            {
-                for (var j = 0; j < col; j++)
-                {
-                    var glyphIndex = tf.CharacterToGlyphMap[arr[i, j][0]];
-                    var geometry = tf.GetGlyphOutline(glyphIndex, unit);
-                    var advanceWidth = tf.AdvanceWidths[glyphIndex] * unit;
-                    var baseline = tf.Baseline * unit;
-                    var mini= geometry.Figures.ToString(x, y + baseline);
-
-                    svg.AppendLine($"<path d='{miniLanguage}' fill='black' stroke='black' stroke-width='0' />");
-                    x += advanceWidth;
-                }
-            }
-            foreach (var uni in unis)
-            {
-                var glyphIndex = tf.CharacterToGlyphMap[uni];
-                var geometry = tf.GetGlyphOutline(glyphIndex, unit);
-                var baseline = tf.Baseline * unit;
-                var advanceWidth = tf.AdvanceWidths[glyphIndex] * unit;
-                var mini = geometry.Figures.ToString(x, baseline);
-                var pathStr = $"<path d=\"{mini}\"/>";
-                sb.AppendLine(pathStr);
-                x += advanceWidth;
-            }
-            System.Diagnostics.Debug.WriteLine(sb);
+            Console.WriteLine("success");
+            Console.ReadLine();
         }
     }
 }
